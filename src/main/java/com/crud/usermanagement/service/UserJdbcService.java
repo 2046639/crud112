c94627f6bda9308596c08ed2d4c254352ad286e9
Gary <startex@ya.ru>
2020-06-17 15:21:08 +0300

    Initial commit

---
+++ src/main/java/com/crud/usermanagement/service/UserJdbcService.java
@@ -0,0 +1,46 @@
+package com.crud.usermanagement.service;
+
+import com.crud.usermanagement.dao.UserJdbcDAO;
+import com.crud.usermanagement.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserJdbcService {
+    private final UserJdbcDAO dao = UserJdbcDAO.getInstance();
+
+    private UserJdbcService() {
+    }
+
+    public static UserJdbcService INSTANCE;
+
+    public static UserJdbcService getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new UserJdbcService();
+        }
+        return INSTANCE;
+    }
+
+
+    public List<User> selectAllUsers() throws SQLException {
+        return dao.selectAllUsers();
+    }
+
+    public User selectUser(int id) throws SQLException {
+        return dao.selectUser(id);
+    }
+
+    public void insertUser(User user) throws SQLException {
+//        dao.dropTable();
+//        dao.createTable();
+        dao.insertUser(user);
+    }
+
+    public boolean updateUser(User user) throws SQLException {
+        return dao.updateUser(user);
+    }
+
+    public boolean deleteUser(int id) throws SQLException {
+        return dao.deleteUser(id);
+    }
+}
---
+++ src/main/java/com/crud/usermanagement/servlet/UserServlet.java
@@ -0,0 +1,120 @@
+package com.crud.usermanagement.servlet;
+
+import com.crud.usermanagement.model.User;
+import com.crud.usermanagement.service.UserJdbcService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Этот Сервлет распределяет запросы пользователей по страницам,
+ * выступает в роли контроллера
+ */
+
+@WebServlet("/")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserJdbcService userService;
+	
+	public void init() {
+
+		userService = UserJdbcService.getInstance();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+
+		try {
+			switch (action) {
+			case "/new":
+				showNewForm(request, response);
+				break;
+			case "/insert":
+				insertUser(request, response);
+				break;
+			case "/delete":
+				deleteUser(request, response);
+				break;
+			case "/edit":
+				showEditForm(request, response);
+				break;
+			case "/update":
+				updateUser(request, response);
+				break;
+			default:
+				listUser(request, response);
+				break;
+			}
+		} catch (SQLException ex) {
+			throw new ServletException(ex);
+		}
+	}
+
+	private void listUser(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+		List<User> listUser = userService.selectAllUsers();
+		request.setAttribute("listUser", listUser);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-list.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		User existingUser = userService.selectUser(id);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+		request.setAttribute("user", existingUser);
+		dispatcher.forward(request, response);
+
+	}
+
+	private void insertUser(HttpServletRequest request, HttpServletResponse response) 
+			throws SQLException, IOException {
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String country = request.getParameter("country");
+		User newUser = new User(name, email, country);
+		userService.insertUser(newUser);
+		response.sendRedirect("list");
+	}
+
+	private void updateUser(HttpServletRequest request, HttpServletResponse response) 
+			throws SQLException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String country = request.getParameter("country");
+
+		User book = new User(id, name, email, country);
+		userService.updateUser(book);
+		response.sendRedirect("list");
+	}
+
+	private void deleteUser(HttpServletRequest request, HttpServletResponse response) 
+			throws SQLException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		userService.deleteUser(id);
+		response.sendRedirect("list");
+
+	}
+
+}
